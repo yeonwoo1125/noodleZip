72d1c75f8ef7e4848b0b604afce686fa318b4488
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-02 18:52:48 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('port', process.env.PORT || 4444);
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',(req, res)=>{
+    res.render('html/index');
+});
+
+app.listen(app.get('port'),()=>{
+    console.log('http://localhost:'+app.get('port'));
+});
