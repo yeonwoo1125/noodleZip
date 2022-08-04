e0a04bedf23640561eee3bed986b8f793be98b5a
chaewon0228 <s2033@e-mirim.hs.kr>
2022-08-04 13:17:27 +0900

    #24 메모 수정 기능 구현

--- router/memos.js
+++ router/memos.js
@@ -36,21 +36,80 @@ router.post('/:userId',
             const memo = await Memo.create({
                 memoTitle: memoTitle,
                 memoContent: memoContent,
-                userId : userId
+                userId: userId
             });
 
             return res.status(201).send({
                 "memoId": memo.memoId,
                 "memoTitle": memo.memoTitle,
                 "memoContent": memo.memoContent,
-                "userId" : memo.userId
+                "userId": memo.userId
             })
         } catch (e) {
             console.error(e);
         }
     });
 
-    
+
+
+// 메모 수정
+// 200 - 메모가 성공적으로 수정된 경우
+// 404 - user을 찾을 수 없는 경우
+// 409 - userId가 일치하지 않는 경우
+
+router.put('/:memoId/:userId',
+    [
+        check('memoTitle', 'memoTitle is empty').trim().not().isEmpty(),
+        check('memoContent', 'memoContent is empty').trim().not().isEmpty()
+    ],
+    async (req, res) => {
+        const reqError = validationResult(req);
+        if (!reqError.isEmpty()) {
+            return res.status(400).send({
+                message: reqError.array()[0].msg
+            })
+        }
+
+        const { memoTitle, memoContent } = req.body;
+        const userId = req.params.userId;
+        const memoId = req.params.memoId;
+
+        if (!await findByUserId(userId)) {
+            return res.status(404).send({
+                message: 'User not found'
+            });
+        }
+
+        const user = await getUser(userId)
+
+        if (user.userId != userId) {
+            return res.status(409).send({
+                message: 'UserID not match'
+            });
+        }
+
+        try {
+            await Memo.update(
+                {
+                    memoTitle: memoTitle,
+                    memoContent: memoContent
+                },
+                { where: { memoId: memoId } }
+            );
+            const memo = await Memo.findByPk(memoId)
+
+            return res.status(200).send({
+                "memoId": memo.memoId,
+                "memoTitle": memo.memoTitle,
+                "memoContent": memo.memoContent
+            })
+        } catch (e) {
+            console.error(e);
+        }
+    });
+
+
+
 const findByUserId = async (id) => {
     return await User.findByPk(id) !== null;
 }
