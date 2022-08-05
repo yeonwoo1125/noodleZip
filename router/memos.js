a0e36ff4b4313e117ab374a30467bdc1df83efe7
yeonwoo1125 <s2021@e-mirim.hs.kr>
2022-08-05 10:31:10 +0900

    #49 메모 상세 조회 기능 구현

--- router/memos.js
+++ router/memos.js
@@ -1,7 +1,7 @@
 const Memo = require('../models/memo');
 const User = require('../models/user');
 
-const { validationResult, check } = require("express-validator");
+const {validationResult, check} = require("express-validator");
 const router = require('express').Router();
 
 
@@ -24,7 +24,7 @@ router.post('/:userId',
             })
         }
 
-        const { memoTitle, memoContent } = req.body;
+        const {memoTitle, memoContent} = req.body;
         const userId = req.params.userId;
 
         const user = await findByUserId(userId);
@@ -41,12 +41,7 @@ router.post('/:userId',
                 userId: userId
             });
 
-            return res.status(201).send({
-                "memoId": memo.memoId,
-                "memoTitle": memo.memoTitle,
-                "memoContent": memo.memoContent,
-                "userId": memo.userId
-            })
+            return res.status(201).send("<script>alert('메모가 작성 되었습니다.'); location.href='" + userId + "' </script>")
         } catch (e) {
             console.error(e);
         }
@@ -72,7 +67,7 @@ router.put('/:memoId/:userId',
             })
         }
 
-        const { memoTitle, memoContent } = req.body;
+        const {memoTitle, memoContent} = req.body;
         const userId = req.params.userId;
         const memoId = req.params.memoId;
 
@@ -83,7 +78,8 @@ router.put('/:memoId/:userId',
             });
         }
 
-        if (!await findByMemoId(memoId)) {
+        let memo = await findByMemoId(memoId);
+        if (memo === null) {
             return res.status(404).send({
                 message: 'memoId not found'
             });
@@ -102,10 +98,11 @@ router.put('/:memoId/:userId',
                     memoContent: memoContent
                 },
                 {
-                    where: { memoId: memoId }
+                    where: {memoId: memoId}
                 }
             );
-            const memo = findByMemoId(memoId);
+
+            memo = await findByMemoId(memoId);
             return res.status(200).send({
                 "memoId": memo.memoId,
                 "memoTitle": memo.memoTitle,
@@ -143,7 +140,7 @@ router.delete('/:memoId/:userId',
         const memo = await findByMemoId(memoId);
         if (memo === null) {
             return res.status(404).send({
-                message: 'memoId not found'
+                message: 'MemoId not found'
             });
         }
 
@@ -155,17 +152,18 @@ router.delete('/:memoId/:userId',
 
         try {
             await Memo.destroy(
-                { where: { memoId: memoId } }
-            )    
+                {where: {memoId: memoId}}
+            )
             return res.status(200).send({
-                message : 'Delete Memo'
+                message: 'Delete Memo'
             });
         } catch (e) {
             console.error(e);
         }
     });
 
-router.get('/:userId',async (req, res)=>{
+
+router.get('/:userId', async (req, res) => {
     const userId = req.params.userId;
     const user = await findByUserId(userId)
     if (user === null) {
@@ -176,24 +174,68 @@ router.get('/:userId',async (req, res)=>{
 
     try {
         const memos = await Memo.findAll({
-        attributes : [
-            'memoId','memoTitle','memoContent'
-        ],
-            where : {userId : userId}
+            attributes: [
+                'memoId', 'memoTitle', 'memoContent', 'userId'
+            ],
+            where: {userId: userId}
         });
 
-        return res.status(200).json(memos);
-    }catch (e) {
+        return res.status(200).render('html/list', {memos});
+    } catch (e) {
         console.error(e);
     }
+});
+
+//메모 상세 조회
+/*
+* 200 - 조회 성공
+* 404 - 유저 및 메모 없음
+* 409 - 유저가 쓴 메모가 아님
+* */
+
+router.get('/:memoId/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const memoId = req.params.memoId;
+
+    const user = await findByUserId(userId)
+    if (user === null) {
+        return res.status(404).send({
+            message: 'User not found'
+        });
+    }
+
+    const memo = await findByMemoId(memoId);
+    if (memo === null) {
+        return res.status(404).send({
+            message: 'Memo not found'
+        });
+    }
+
+    const memoByUser = await findByUserIdAndMemoId(userId, memoId);
+    if(memoByUser.length === 0){
+        return res.status(409).send({
+            message : 'Not a user-written note'
+        })
+    }
+
+    return res.status(200).send({
+        memoTitle : memo.memoTitle,
+        memoContent : memo.memoContent
+    });
 });
 
 const findByUserId = async (id) => {
     return await User.findByPk(id);
 }
 
-const findByMemoId = async (id)=>{
+const findByMemoId = async (id) => {
     return await Memo.findByPk(id);
+}
+
+const findByUserIdAndMemoId = async (userId, memoId) => {
+    return await Memo.findAll({
+        where: {userId: userId, memoId: memoId}
+    });
 }
 
 module.exports = router;
